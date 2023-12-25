51d345e76efbdc5431d0b9c4fc93ca4bbb72eb9b
kishlaykiku <kishlaykumarkkp24@gmail.com>
2023-12-25 20:50:01 +0530

    bhut sara changes

---
+++ script/auspicious.js
@@ -0,0 +1,38 @@
+// Update the count down every 1 second
+function updateTime() {
+
+    // Set the date we're counting down to
+    let target_date = new Date("Jan 30, 2024 23:59:59").getTime();
+
+    // Get current date and time
+    let current_date = new Date().getTime();
+        
+    // Find the difference between current and the target date
+    let time_difference = target_date - current_date;
+        
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(time_difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((time_difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((time_difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((time_difference % (1000 * 60)) / 1000);
+        
+    // Output the result
+    $('.days').text(days);
+    $('.hours').text(hours);
+    $('.minutes').text(minutes);
+    $('.seconds').text(seconds);
+    
+    // If the count down is over, write some text 
+    if (time_difference < 0) {
+
+        let message = '<div class="timer-end-message"><p>Event has Completed</p></div>';
+
+        // Clear all intervals to avoid repetation
+        clearInterval(keep_track);
+
+        $('.unit-placeholder').remove();
+        $('.timer').append(message);
+    }
+}
+
+let keep_track = setInterval(updateTime, 1000);
--- script/home.js
+++ script/home.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
+
+    // Store image URLs
     let images = ['../images/home/home1.jpg', '../images/home/home2.jpg', '../images/home/home3.jpg']
     let curr_index = 0;
 
-
+    // Preload all images
     function preloadImages(images) {
         let img;
         for (var i = 0; i < images.length; i++) {
@@ -12,7 +14,7 @@ $(document).ready(function () {
     }
     preloadImages(images);
 
-
+    // Change the background every {specified time}
     function cycleBack() {
         $('.home-section').css('background-image', 'url(' + images[curr_index] + ')');
 
