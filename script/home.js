de0b4530761ce1d1f85ebc7718cea98937839501
kishlaykiku <kishlaykumarkkp24@gmail.com>
2024-01-02 17:32:26 +0530

    Final Commit iss baar pakka

--- script/home.js
+++ script/home.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
 
     // Store image URLs
-    let images_mobile = ['./images/home/home1m.jpg', './images/home/home2m.jpg', './images/home/home3m.jpg']
-    let images_desktop = ['./images/home/home1p.jpg', './images/home/home2p.jpg', './images/home/home3p.jpg']
+    let images_mobile = ['./images/home/home1m.jpg', './images/home/home2m.jpg', './images/home/home3m.jpg', './images/home/home4m.jpg'];
+    let images_desktop = ['./images/home/home1p.jpg', './images/home/home2p.jpg', './images/home/home3p.jpg', './images/home/home4p.jpg'];
     let curr_index = 0;
+
+    let width = screen.width;
+
+    if(width < 1101) {
+        $('.home-section').css('background-image', `url(${images_mobile[0]})`);
+    }
+    else {
+        $('.home-section').css('background-image', `url(${images_desktop[0]})`);
+    }
 
     // Change the background every {specified time}
     function cycleBack() {
 
-        let width = screen.width;
+        width = screen.width;
 
         if(width < 1101) {
             $('.home-section').css('background-image', `url(${images_mobile[curr_index]})`);
